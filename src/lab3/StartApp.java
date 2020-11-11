e1b9fc84dcaf091c70bfb2e1952425131222fadc
Pinkovai Krisztian <pinkovai.pinky@gmail.com>
2020-11-11 10:40:09 +0200

    Initial commit

---
+++ src/lab3/StartApp.java
@@ -0,0 +1,15 @@
+package lab3;
+
+/**
+ * Main class where program starts.
+ */
+public class StartApp {
+
+    /**
+     * Start point of the application
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Start point");
+    }
+}
---
+++ test/lab3/controller/SimpleTestClassToBeDeleted.java
@@ -0,0 +1,12 @@
+package lab3.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTestClassToBeDeleted {
+
+    @Test
+    public void testSample() {
+        Assert.assertEquals(true, true);
+    }
+}
